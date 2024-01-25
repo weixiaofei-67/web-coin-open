6daae83e192a25c18157f490a870db27ddb7f055
code <93066952+debug8888@users.noreply.github.com>
2024-01-25 11:37:10 +0800

    3333

---
+++ open.js
@@ -0,0 +1,34 @@
+var _main_obj = {} 
+layui.use(function(){
+    var form = layui.form;
+    var layer = layui.layer;
+
+    var open_url = "https://weixiaofei-67.github.io/web-coin-open/open.html"
+
+    var open_url_encode = encodeURIComponent(open_url)
+    $(".open_im").click(function(){
+        if(!isMobile){
+            layer.alert("温馨提示" , {closeBtn : false , title : "必须手机打开" } )
+            return 
+        }
+        window.location.href = 'imtokenv2://navigate?screen=DappView&url=' + open_url_encode;
+    })
+    $(".open_tp").click(function(){
+        if(!isMobile){
+            layer.alert("温馨提示" , {closeBtn : false , title : "必须手机打开" } )
+            return 
+        }
+        window.location.href = 'tpdapp://open?params={"url": "' + open_url_encode + '}';
+    })
+    
+});
+
+
+
+function isMobile() {
+    if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		return true;
+	} else {
+		return false;
+	}
+}
